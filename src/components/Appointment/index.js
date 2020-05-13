5ce6613123c37e6d7a57acd1f0c95c701c4f5c7b
cameron cheng <cameroncheng@gmail.com>
2020-05-13 12:55:13 -0700

    create confirm.js, empty.js, show.js, header.js

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./style.scss"
+const  classnames = require('classnames');
+
+
+export default function Appointment(props) {
+  return(
+  <article className="appointment">
+
+  </article>
+  )
+}
