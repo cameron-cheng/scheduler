1020fd1c96b61b56b7599c08ce16289af3e8df80
cameron cheng <cameroncheng@gmail.com>
2020-05-16 17:06:26 -0700

    create useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  const back = () => {
+   if(history.length < 2){
+    return; 
+   };
+    setHistory(prev => [...prev.slice(0, prev.length -1)]);
+  }
+  const transition = (newMode, replace = false) => {
+    setHistory( prev => replace ? [...prev.slice(0, prev.length -1), newMode] : [...prev, newMode])
+  }
+  return{ mode: history[history.length-1], transition, back };
+}
